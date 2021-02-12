ea7b8293537916a4aaebbdc00e586533b380a41f
Anderson Jimenez <andersonmurdog@gmail.com>
2021-02-12 00:29:31 -0500

    se crea servicvios y botones de home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  irPokemones(){
+    console.log('Ir a irPokemones');
+    this.router.navigate(['/pokemon']);
+  }
+
+  irHabitatPokemon(){
+    console.log('Ir a irHabitatPokemon');
+    this.router.navigate(['/pokemon-habitat']);
+  }
+
+  irFormasPokemon(){
+    console.log('Ir a irHabitatPokemon');
+    this.router.navigate(['/pokemon-form']);
+  }
 
 }
